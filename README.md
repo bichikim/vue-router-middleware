e827fccc0375525f9994ca88087f580374ec1264
Bichi Kim <bichi@live.co.kr>
2019-04-03 13:07:36 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vue Router Middleware
+> vue router middleware
+
+[![LICENSE IMAGE]](https://www.npmjs.org/package/vue-router-middleware)
+![npm](https://img.shields.io/npm/v/vue-router-mw.svg)
+![Codecov](https://img.shields.io/codecov/c/github/bichikim/vue-router-middleware.svg)
+![Travis](https://img.shields.io/travis/bichikim/vue-router-middleware.svg)
+
+[LICENSE IMAGE]:https://img.shields.io/npm/l/vue-router-middleware.svg
+[NPM LINK]:https://www.npmjs.org/package/vue-router-middleware
+
+
---
+++ config/webpack.base.config.js
@@ -0,0 +1,56 @@
+/* tslint:disable:no-require-imports */
+const path = require('path')
+const formatter = require('eslint-friendly-formatter')
+
+const resolve = (dir) => {
+  return path.join(__dirname, '..', dir)
+}
+// noinspection JSUnusedGlobalSymbols
+module.exports = {
+  target: 'node',
+  entry: {
+    app: ['./src/index.ts'],
+  },
+  output: {
+    path: resolve('dist'),
+    filename: '[name].js',
+    pathinfo: true,
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      '@': resolve('src'),
+      '~': resolve('lib'),
+      '@@': resolve('./'),
+      '~~': resolve('./'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|ts|vue)/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        options: {
+          formatter,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.ts$/, exclude: /node_modules/, use: [
+          {
+            loader: 'ts-loader', options: {
+              appendTsSuffixTo: [/\.vue$/],
+            },
+          },
+
+        ],
+      },
+    ],
+  },
+}
